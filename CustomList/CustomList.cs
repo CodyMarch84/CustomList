f0a139fc6893b54179a831e44ff9511d05f45bc9
Cody March <cody.march84@gmail.com>
2018-01-16 08:04:43 -0600

    added CustomList class and code for it

---
+++ CustomList/CustomList.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomList
+{
+    class CustomList<T>
+    {
+        // HOW TO OVERLOAD + (PLUS) OPERATOR
+        public T[] array;
+        private int count;
+        public int capacity;
+
+        public int Count
+        {
+            get
+            {
+                return count;
+            }
+        }
+
+
+        public CustomList()
+        {
+            count = 0;
+            capacity = 5;
+            array = new T[capacity];
+        }
+
+
+        public void Add(T item)
+        {
+            // if statement - check to see if you have room to add
+            if (count >= capacity)
+            {
+                capacity *= 2;
+                T[] tempArray = new T[capacity];
+                // fill in tempArray (loop over 'array' to fill 
+                // original values into 'tempArray')
+                for (int i = 0; i < count; i++)
+                {
+                    tempArray[i] = array[i];
+                }
+                tempArray[count] = item;
+                array = tempArray;
+            }
+            else
+            {
+                // add to list (inner array)
+                array[count] = item;
+            }
+            count++;
+        }
+    }
+}
--- CustomList/Program.cs
+++ CustomList/Program.cs
@@ -10,6 +10,18 @@ namespace CustomList
     {
         static void Main(string[] args)
         {
+            // List<int> list = new List<int>();
+
+            CustomList<int> customList = new CustomList<int>();
+
+            // list.Add(5);
+
+            customList.Add(1);
+            customList.Add(2);
+            customList.Add(3);
+            customList.Add(4);
+            customList.Add(5);
+            customList.Add(6);
         }
     }
 }
