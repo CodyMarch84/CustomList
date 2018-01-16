17b655c1477f451c46f1479bbec1bbb55fb98676
Cody March <cody.march84@gmail.com>
2018-01-16 00:02:28 -0600

    added code for addition class

--- CustomList/Addition.cs
+++ CustomList/Addition.cs
@@ -8,5 +8,45 @@ namespace CustomList
 {
     class Addition
     {
+        public int addTwoNumbers(int firstValue, int secondValue)
+        {
+            displayFormula(firstValue, secondValue);
+            int result = firstValue + secondValue;
+            return result;
+        }
+
+        public void displayFormula(int numberOne, int numberTwo)
+        {
+            Console.WriteLine(numberOne + " + " + numberTwo);
+        }
+
+        public int GetUserInput()
+        {
+            int result;
+            string response = Console.ReadLine();
+
+            try
+            {
+                result = Int32.Parse(response);
+            }
+            catch
+            {
+                Console.WriteLine("Oops, you suck, invalid entry, try again!"); // need a number, 1-10
+                return GetUserInput();
+            }
+
+            // BELOW - alternative example of validation of user input
+
+            // bool isNumber = Int32.TryParse(response, out result);
+            // while (!isNumber)
+            // {
+            //     Console.WriteLine("Oops, you suck, invalid entry, try again!");
+            //     isNumber = Int32.TryParse(response, out result);
+            // }
+
+            return result;
+        }
+    }
+}
     }
 }
